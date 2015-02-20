a5f5662e46d8cd78e145ed0999f9826086461728
innovator256 <BM-2cV87rXv7cVFLWZGnSJN14BWHHrG9Aj7Zr@bitmessage.ch>
2015-02-20 07:03:18 -0500

    quick server

--- app/server.js
+++ app/server.js
@@ -1,2 +1,29 @@
 var express = require('express'),
     app = express();
+
+app.configure(function () {
+              app.use(express.static(__dirname, '/'));
+});
+
+
+app.get('/donors/:d', function(req, res){
+    var donorId = parseInt(req.params.id);
+    var data = {};
+    for (var i =0, len=donors.length;i<len;i++){
+        if (donors[i].id === donorId){
+            data = donors[i];
+            break;
+        }
+        
+    }
+    res.json(data);
+});
+
+app.get('/donors', function (req,res){
+    res.json(donors);
+    
+});
+
+app.listen(8080);
+
+console.log("express listening on port 8080");
