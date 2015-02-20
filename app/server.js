058ca025cac9b962bdb003cf3878d9ed2452fb2f
innovator256 <BM-2cV87rXv7cVFLWZGnSJN14BWHHrG9Aj7Zr@bitmessage.ch>
2015-02-20 07:06:46 -0500

    server service

--- app/server.js
+++ app/server.js
@@ -27,3 +27,42 @@ app.get('/donors', function (req,res){
 app.listen(8080);
 
 console.log("express listening on port 8080");
+
+//quick server request test
+
+       var donors = [
+            
+            {
+                id : 1,
+                joined: "2000-12-01",
+                address: "1e2wfrf044g",
+                token: 10000000,
+                amount:"20005btc"
+            },
+            
+            {
+                id : 2,
+                joined: "2002-2-02",
+                address: "1d4254frg4",
+                token: 100000,
+                amount:"50btc"
+            },
+            
+            {
+                id : 3,
+                joined: "2003-12-09",
+                address: "134rr3p1rg0",
+                token: 1000000,
+                amount:"100btc"
+            },
+            
+            {
+                id : 4,
+                joined: "2004-1-06",
+                address: "134rr3p1rg0",
+                token: 1000,
+                amount:"10btc"
+            }
+        
+        ];
+        
